139de8268ec45f8b34e9563fbe004ab303af439e
Lightm0re <acermsp@gmail.com>
2017-10-29 23:31:57 +1000

    Update files.py
    переделал счетчик букв

--- files.py
+++ files.py
@@ -1,13 +1,13 @@
 def result():
     with open('text', 'r') as my_file:
         my_string = my_file.read()
-        count = 0
-        for i, j in enumerate(my_string):
-                if j != ' ' and j != '.':
-                    count += 1
+        a = my_string.replace(' ', '')
+        b = a.replace('.', '')
+        count = b.replace('\n', '')
+        letters = str(len(count)) + ' letters\n'
         words = str(len(my_string.split())) + ' words\n'
         lines = str(len(my_string.splitlines())) + ' lines\n'
-        letters = str(count) + ' letters\n'
+
     with open('result', 'w') as my_result:
         my_result.write(letters)
         my_result.write(words)
