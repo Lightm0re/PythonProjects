2e1d069e553f03e6293a30dcb15ab0d9243fd55e
Lightm0re <acermsp@gmail.com>
2017-10-29 21:28:12 +1000

    Update files.py
    убрал лишние write

--- files.py
+++ files.py
@@ -5,16 +5,13 @@ def result():
         for i, j in enumerate(my_string):
                 if j != ' ' and j != '.':
                     count += 1
-        words = str(len(my_string.split())) + ' words'
-        lines = str(len(my_string.splitlines())) + ' lines'
-        letters = str(count) + ' letters'
+        words = str(len(my_string.split())) + ' words\n'
+        lines = str(len(my_string.splitlines())) + ' lines\n'
+        letters = str(count) + ' letters\n'
     with open('result', 'w') as my_result:
         my_result.write(letters)
-        my_result.write('\n')
         my_result.write(words)
-        my_result.write('\n')
         my_result.write(lines)
-        my_result.write('\n')
 
 
 result()
