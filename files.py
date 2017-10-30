753c9faba87c66450c9fdbdd4f5e5f7140123272
Lightm0re <acermsp@gmail.com>
2017-10-30 15:03:29 +1000

    Update files.py
    добавил import re и исключение

--- files.py
+++ files.py
@@ -1,17 +1,23 @@
-def result():
-    with open('text', 'r') as my_file:
-        my_string = my_file.read()
-        a = my_string.replace(' ', '')
-        b = a.replace('.', '')
-        count = b.replace('\n', '')
-        letters = str(len(count)) + ' letters\n'
-        words = str(len(my_string.split())) + ' words\n'
-        lines = str(len(my_string.splitlines())) + ' lines\n'
+import re
 
-    with open('result', 'w') as my_result:
-        my_result.write(letters)
-        my_result.write(words)
-        my_result.write(lines)
+
+def result():
+
+    try:
+        with open('text', 'r') as my_file:
+            my_string = my_file.read()
+    except FileNotFoundError:
+        print('Тоби пизда, тика с села')
+    else:
+            strings = re.sub(r'\W', '', my_string)
+            letters = str(len(strings)) + ' letters\n'
+            words = str(len(my_string.split())) + ' words\n'
+            lines = str(len(my_string.splitlines())) + ' lines\n'
+            with open('result', 'w') as my_result:
+                my_result.write(letters)
+                my_result.write(words)
+                my_result.write(lines)
+                my_result.close()
 
 
 result()
