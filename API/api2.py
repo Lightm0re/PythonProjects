c037087f7f3e11fcf9718a088f3324d4baa1bd28
Lightm0re <acermsp@gmail.com>
2017-11-28 00:49:17 +1000

    Убрал конкатенацию

--- API/api2.py
+++ API/api2.py
@@ -2,11 +2,12 @@
 выдать краткую информацию о нем. """
 import urllib.request
 import json
+url = 'http://pokeapi.co/api/v2/pokemon/{}'
 headers = {}
 n = input('Введите номер покемона')
-headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-x = urllib.request.Request('http://pokeapi.co/api/v2/pokemon/' + n, headers=headers)
+headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 " \
+                        "Safari/537.17 "
+x = urllib.request.Request(url.format(n), headers=headers)
 resp = urllib.request.urlopen(x)
 obj = json.loads(resp.read())
 print(obj)
-
