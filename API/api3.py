6e75738a1f88fb6578fa0c3a9403699dd02aa797
Lightm0re <acermsp@gmail.com>
2017-11-28 00:47:22 +1000

    Убрал конкатенацию

--- API/api3.py
+++ API/api3.py
@@ -2,12 +2,13 @@
 продуктов, получаем рецепты для этого списка продуктов """
 import urllib.request
 import json
+url = 'http://www.recipepuppy.com/api/?i={}'
 headers = {}
 n = input('Введите ингридиенты (через запятую)')
-n.replace(',', '%2C+')
-headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-x = urllib.request.Request('http://www.recipepuppy.com/api/?i=' + n, headers=headers)
+m = n.replace(',', '%2C+')
+headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 " \
+                        "Safari/537.17 "
+x = urllib.request.Request(url.format(m), headers=headers)
 resp = urllib.request.urlopen(x)
 obj = json.loads(resp.read())
 print(obj)
-
